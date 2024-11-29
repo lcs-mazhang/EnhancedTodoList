dd6d10a2d550680ba6493abf3425ba1dfb70aa32
Matthew Zhang <gzhang@lcs.on.ca>
2024-11-29 03:10:29 -0500

    Added a scrollable list to show to-do items.

--- EnhancedTodoList/Views/TodoListView.swift
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct TodoListView: View {
+    
+    // MARK: Stored properties
+    
+    // Our list of items to complete
+    @State private var items: [TodoItem] = []
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("hello")
+        NavigationStack {
+            VStack {
+                
+                List(items) { currentItem in
+                    Label {
+                        Text(currentItem.details)
+                    } icon: {
+                        Image(systemName: currentItem.isCompleted ? "checkmark.circle" : "circle")
+                    }
+                }
+            }
+            .navigationTitle("Tasks")
+        }
+        .onAppear {
+            // Populate with example data
+            if items.isEmpty {
+                items.append(contentsOf: exampleData)
+            }
+        }
     }
+    
 }
 
 #Preview {
