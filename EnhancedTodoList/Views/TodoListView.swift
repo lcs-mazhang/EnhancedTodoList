afc3dbb2d29f3e3fa6abf5b54dd9a6f302254a9b
Matthew Zhang <gzhang@lcs.on.ca>
2024-11-29 03:22:36 -0500

    Made it possible to add new items to the list.

--- EnhancedTodoList/Views/TodoListView.swift
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -11,6 +11,9 @@ struct TodoListView: View {
     
     // MARK: Stored properties
     
+    // The item currently being created
+    @State private var newItemDetails = ""
+    
     // Our list of items to complete
     @State private var items: [TodoItem] = []
     
@@ -20,6 +23,17 @@ struct TodoListView: View {
             VStack {
                 
                 VStack {
+                    
+                    HStack {
+                        
+                        TextField("Enter a to-do item", text: $newItemDetails)
+                        
+                        Button("Add") {
+                            addItem()
+                        }
+                     
+                    }
+                    .padding(20)
                     
                     if items.isEmpty {
                         
@@ -54,6 +68,13 @@ struct TodoListView: View {
                 }
             }
         }
+    }
+    
+    // MARK: Functions
+    func addItem() {
+        let newToDoItem = TodoItem(details: newItemDetails)
+        items.insert(newToDoItem, at: 0)
+        newItemDetails = ""
     }
 }
 
